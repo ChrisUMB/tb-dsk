5441a75ac0afbc36081301a03e9ece528b818963
Tom <timtimtim7@gmail.com>
2023-03-18 14:34:23 -0500

    Added rgb_to_hsv function

--- util.lua
+++ util.lua
@@ -199,6 +199,39 @@ function hsv_to_rgb(h, s, v)
     return vec3(r, g, b)
 end
 
+function rgb_to_hsv(r, g, b)
+    local max = math.max(r, g, b)
+    local min = math.min(r, g, b)
+    local h, s, v
+
+    v = max
+
+    local d = max - min
+    if max == 0 then
+        s = 0
+    else
+        s = d / max
+    end
+
+    if max == min then
+        h = 0
+    else
+        if max == r then
+            h = (g - b) / d
+            if g < b then
+                h = h + 6
+            end
+        elseif max == g then
+            h = (b - r) / d + 2
+        elseif max == b then
+            h = (r - g) / d + 4
+        end
+        h = h / 6
+    end
+
+    return vec3(h, s, v)
+end
+
 ---@param x number The x position of the quad.
 ---@param y number The y position of the quad.
 ---@param width number The width of the quad.

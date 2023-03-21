33885e5dfecab750704feccfc59e67af1b6f7448
ChrisUMB <kchrisumb@gmail.com>
2023-03-20 20:04:15 -0700

    Refactored "Events" to "Hooks" due to conflicting Toribash class, usage of new EmmyLua Annotations

--- events.lua
+++ events.lua
@@ -1,34 +1,81 @@
-local listen
+---@class Hook
 
----@class Event
-
----@generic T : Event
----@class EventType<T>
+---@generic T : Hook
+---@class HookType<T>
 ---@field id string The ID of the event type
 
----@generic T : Event
----@type EventType<T>
+---@generic T : Hook
+---@type HookType<T>
 local EventType = {}
+
 EventType.__index = EventType
 
----@generic T : Event
----@param self EventType<T>
----@param hook_id string The ID of the hook
----@param callback fun(event:T) The callback to call when the event is fired
-function EventType.listen(self, hook_id, callback)
+---@class Listener
+---@field id string The ID of the listener
+---@field hook_type HookType The event to listen for
+local Listener = {}
+Listener.__index = Listener
 
+---@type Listener[]
+local listeners = {}
+
+---@return Listener
+local function listener_new(event, id)
+    local l = {
+        id = id,
+        event = event
+    }
+
+    setmetatable(l, Listener)
+    return l
 end
 
----@generic T : Event
----@param self EventType<T>
+--- Cancels the listener
+function Listener:cancel()
+    if listeners[self.hook_type] then
+        listeners[self.hook_type][self.id] = nil
+    end
+end
+
+---@generic T : Hook
+---@param self HookType<T>
 ---@param event T The event to pass to the listeners
 function EventType.call(self, event)
+    if not event then
+        event = {}
+    end
 
+    local hook_listeners = listeners[self]
+    if not hook_listeners then
+        return
+    end
+
+    for listener, callback in pairs(hook_listeners) do
+        local s, e = pcall(function()
+            callback(event, listener)
+        end)
+
+        if not s then
+            println(e)
+        end
+    end
+end
+
+---@generic T : Hook
+---@param self HookType<T>
+---@param listener_id string The ID of the hook
+---@param callback fun(event:T, listener: Listener) The callback to call when the event is fired
+---@return Listener The listener object
+function EventType.listen(self, listener_id, callback)
+    local others = listeners[self] or {}
+    others[listener_id] = callback
+    listeners[self] = others
+    return listener_new(self, listener_id)
 end
 
 ---@param id string The ID of the event
----@return EventType
-local function create_event_type(id)
+---@return HookType
+local function new_hook(id)
     local event_type = {
         id = id
     }
@@ -39,113 +86,49 @@ local function create_event_type(id)
     return event_type
 end
 
----@type table<string, EventType>
-Events = {
-    GAME_START = "game-start", -- Called when a replay or a match has started.
-    GAME_FRAME = "game-frame", -- Called when a replay or a match frame is entered.
-    GAME_END = "game-end", -- Called when a replay or a match is ended.
+---@type table<string, HookType>
+Hooks = {
+    GAME_START = new_hook("game-start"), -- Called when a replay or a match has started.
+    GAME_FRAME = new_hook("game-frame"), -- Called when a replay or a match frame is entered.
+    GAME_END = new_hook("game-end"), -- Called when a replay or a match is ended.
 
-    MATCH_START = "match-start", -- Called when a match starts.
-    MATCH_FRAME = "match-frame", -- Called when a frame is entered during a match.
-    MATCH_END = "match-end", -- Called when a match ends.
-    POST_MATCH_START = "post-match-start", -- Called at the same time as match-end. Start of the frames between the match ending and the replay starting.
-    POST_MATCH_FRAME = "post-match-frame", -- Called when a frame is entered in the post-match stage.
-    POST_MATCH_END = "post-match-end", -- Called at the end of the post match stage.
+    MATCH_START = new_hook("match-start"), -- Called when a match starts.
+    MATCH_FRAME = new_hook("match-frame"), -- Called when a frame is entered during a match.
+    MATCH_END = new_hook("match-end"), -- Called when a match ends.
+    POST_MATCH_START = new_hook("post-match-start"), -- Called at the same time as match-end. Start of the frames between the match ending and the replay starting.
+    POST_MATCH_FRAME = new_hook("post-match-frame"), -- Called when a frame is entered in the post-match stage.
 
-    REPLAY_START = "replay-start", -- Called when a replay is started.
-    REPLAY_FRAME = "replay-frame", -- Called when a frame is entered during a replay.
-    REPLAY_END = "replay-end", -- Called when a replay ends.
+    ---@class PostMatchEndEvent : Hook
+    ---@type HookType<PostMatchEndEvent>
+    --- Called at the end of the post match stage.
+    POST_MATCH_END = new_hook("post-match-end"),
 
-    ---@class WindowResizeEvent : Event
+    REPLAY_START = new_hook("replay-start"), -- Called when a replay is started.
+    REPLAY_FRAME = new_hook("replay-frame"), -- Called when a frame is entered during a replay.
+    REPLAY_END = new_hook("replay-end"), -- Called when a replay ends.
+
+    ---@class WindowResizeEvent : Hook
     ---@field new_size vec2 The new size of the window.
     ---@field old_size vec2 The old size of the window.
-    ---@type EventType<WindowResizeEvent>
+    ---@type HookType<WindowResizeEvent>
     --- Called whenever the window is resized.
-    WINDOW_RESIZE = "window-resize",
+    WINDOW_RESIZE = new_hook("window-resize"),
 
-    ---@class Draw2DEvent : Event
-    ---@type EventType<Draw2DEvent>
+    ---@class Draw2DEvent : Hook
+    ---@type HookType<Draw2DEvent>
     --- Called when the 2D drawing hook is called.
-    DRAW_2D = "draw2d",
+    DRAW_2D = new_hook("draw2d"),
 
-    ---@class KeyDownEvent : Event
+    ---@class KeyDownEvent : Hook
     ---@field x number The x position of the mouse.
     ---@field y number The y position of the mouse.
     ---@field key number The key that was pressed.
-    ---@type EventType<KeyDownEvent>
+    ---@type HookType<KeyDownEvent>
     --- Called whenever a key is pressed.
-    KEY_DOWN = "key-down"
+    KEY_DOWN = new_hook("key-down")
 }
 
----@class listener
----@field id string The ID of the listener
----@field event string The event to listen for
-local listener = {}
-listener.__index = listener
-
----@type listener[]
-local listeners = {}
-
-local function listener_new(event, id)
-    local l = {
-        id = id,
-        event = event
-    }
-
-    setmetatable(l, listener)
-    return l
-end
-
---- Cancels the listener
-function listener:cancel()
-    unlisten(self.event, self.id)
-end
-
----@param event string The event to listen for
----@param id string The ID of the listener
-function unlisten(event, id)
-    if listeners[event] then
-        listeners[event][id] = nil
-    end
-end
-
----@generic T : Event
----@param event EventType<T> The event to listen for
----@param id string The ID of the listener
----@param func fun(event:T) The function to call when the event is fired
----@return listener The listener
-function listen(event, id, func)
-    local others = listeners[event] or {}
-    others[id] = func
-    listeners[event] = others
-    return listener_new(event, id)
-end
-
-Events.WINDOW_RESIZE:listen("dsk-event-system", function(event)
-
-end)
-
----@generic T : Event
----@param event_type EventType<T> The event to call
----@param event T The event to pass to the listeners
-function call_event(event_type, event)
-    local event_listeners = listeners[event_type]
-    if not event_listeners then
-        return
-    end
-
-    for _, listener_func in pairs(event_listeners) do
-        local s, e = pcall(function()
-            listener_func(event)
-        end)
-
-        if not s then
-            println(e)
-        end
-    end
-end
-
-local HOOK_ID = "dsk-event-system"
+local HOOK_ID = "dsk-events"
 remove_hooks(HOOK_ID)
 
 local MODE_MATCH = 0
@@ -158,11 +141,6 @@ local MODE = {
 local match_over = false
 local post_match_over = false
 
-local previous_frame = get_world_state().match_frame
--- Using pre_draw eliminated frame delta variance with lower framerates
--- and seems more consistent overall.
-
-local last_size = vec2(get_window_size())
 
 add_hook("enter_frame", HOOK_ID, function()
     local state = get_world_state()
@@ -173,28 +151,28 @@ add_hook("enter_frame", HOOK_ID, function()
         if mode == MODE_REPLAY then
             if not post_match_over then
                 post_match_over = true
-                call_event("post-match-end", state)
+                Hooks.POST_MATCH_END:call()
             end
 
-            call_event("replay-start", state)
+            Hooks.REPLAY_START:call()
         end
     end
 
-    call_event("game-frame", state)
+    Hooks.GAME_FRAME:call()
 
     if not post_match_over and match_over then
-        call_event("post-match-frame", state)
+        Hooks.POST_MATCH_FRAME:call()
     elseif mode == MODE_REPLAY then
-        call_event("replay-frame", state)
+        Hooks.REPLAY_FRAME:call()
     elseif mode == MODE_MATCH then
-        call_event("match-frame", state)
+        Hooks.MATCH_FRAME:call()
     end
-
-    previous_frame = frame
 end)
 
+local last_size = vec2(get_window_size())
+
 add_hook("resolution_changed", HOOK_ID, function()
-    call_event(Events.WINDOW_RESIZE, {
+    Hooks.WINDOW_RESIZE:call({
         new_size = vec2(get_window_size()),
         old_size = last_size
     })
@@ -248,12 +226,11 @@ end)
 --end)
 
 add_hook("leave_game", HOOK_ID, function()
-    local state = get_world_state()
-    local mode = state.replay_mode
-    if mode ~= MODE_REPLAY then
+    if get_world_state().replay_mode ~= MODE_REPLAY then
         return
     end
-    call_event("replay-end", state)
+
+    Hooks.REPLAY_END:call()
 end)
 
 add_hook("end_game", HOOK_ID, function()
@@ -261,18 +238,20 @@ add_hook("end_game", HOOK_ID, function()
         return
     end
     match_over = true
-    local state = get_world_state()
-    call_event("match-end", state)
-    call_event("post-match-start", state)
+    Hooks.MATCH_END:call()
+    Hooks.POST_MATCH_START:call()
 end)
 
 add_hook("match_begin", HOOK_ID, function()
     match_over = false
     post_match_over = false
-    local state = get_world_state()
-    call_event("match-start", state)
+    Hooks.MATCH_START:call()
 end)
 
 add_hook("draw2d", HOOK_ID, function()
-    call_event(EVENTS.DRAW2D)
+    Hooks.DRAW_2D:call()
+end)
+
+Hooks.REPLAY_FRAME:listen("test", function()
+
 end)
